72fe4d225089e30f80d4cb5f039dc09ab91a5e84
Pavel Sobolev <paveloomm@gmail.com>
2021-05-10 15:39:02 +0300

    Delete the `Test Build` workflow. Test `sed` without `-i`.

--- .github/scripts/fleek/hooks.bash
+++ .github/scripts/fleek/hooks.bash
@@ -4,5 +4,5 @@
 # Expects to be used as part of the build process for Fleek.
 
 # Avoid Email Adress Obfuscation
-sed -i 's|>paveloomm@gmail.com<|><!--email_off-->paveloomm@gmail.com<!--/email_off--><|g' public/index.html
+sed 's|>paveloomm@gmail.com<|><!--email_off-->paveloomm@gmail.com<!--/email_off--><|g' public/index.html
 sed -i 's|user@ip|<!--email_off-->user@ip<!--/email_off-->|g' public/pensieve/how-to-ssh-into-wsl-2/index.html
