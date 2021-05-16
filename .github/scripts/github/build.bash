265fbe967d3a0794ea1d3175e99604b5b0c6f18f
Pavel Sobolev <paveloomm@gmail.com>
2021-05-16 12:59:47 +0300

    Delete binaries when done.

--- .github/scripts/github/build.bash
+++ .github/scripts/github/build.bash
@@ -13,6 +13,9 @@ else
     ./zola build -u "${1}"
 fi
 
+# Delete Zola
+rm zola
+
 # Get `minify-html`
 wget https://wilsonl.in/minify-html/bin/0.4.10-linux-x86_64 >/dev/null 2>&1
 mv *linux-x86_64 minify-html
@@ -23,9 +26,15 @@ for file in `find public -name "*.html" -type f`; do
     ./minify-html -s "$file" -o "$file" --css --js
 done
 
+# Delete `minify-html`
+rm minify-html
+
 # Get `minify`
 wget https://github.com/tdewolff/minify/releases/download/v2.9.16/minify_linux_amd64.tar.gz >/dev/null 2>&1
 tar -xf minify_* minify && rm minify_*
 
 # Use `minify`
 ./minify -ra -o . public
+
+# Delete `minify`
+rm minify
