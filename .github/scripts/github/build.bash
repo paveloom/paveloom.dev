d81081a92c5c4babde322c80ac4c9acfb6b18975
Pavel Sobolev <paveloom@tuta.io>
2021-07-20 16:09:55 +0300

    Update to Zola `0.14.0`; disable the color inversion.

--- .github/scripts/github/build.bash
+++ .github/scripts/github/build.bash
@@ -3,7 +3,7 @@
 # A script to build the site for GitHub Pages.
 
 # Get Zola
-wget https://github.com/getzola/zola/releases/download/v0.13.0/zola-v0.13.0-x86_64-unknown-linux-gnu.tar.gz >/dev/null 2>&1
+wget https://github.com/getzola/zola/releases/download/v0.14.0/zola-v0.14.0-x86_64-unknown-linux-gnu.tar.gz >/dev/null 2>&1
 tar -xf zola*.tar.gz && rm zola-*
 
 # Build the site
@@ -16,21 +16,8 @@ fi
 # Delete Zola
 rm zola
 
-# Get `minify-html`
-wget https://wilsonl.in/minify-html/bin/0.4.10-linux-x86_64 >/dev/null 2>&1
-mv *linux-x86_64 minify-html
-chmod u+x minify-html
-
-# Use `minify-html`
-for file in `find public -name "*.html" -type f`; do
-    ./minify-html -s "$file" -o "$file" --css --js
-done
-
-# Delete `minify-html`
-rm minify-html
-
 # Get `minify`
-wget https://github.com/tdewolff/minify/releases/download/v2.9.16/minify_linux_amd64.tar.gz >/dev/null 2>&1
+wget https://github.com/tdewolff/minify/releases/download/v2.9.19/minify_linux_amd64.tar.gz >/dev/null 2>&1
 tar -xf minify_* minify && rm minify_*
 
 # Use `minify`
