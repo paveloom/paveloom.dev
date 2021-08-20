aea60041cb74461052e5bec8c46af73dfdfd071f
Pavel Sobolev <paveloom@tuta.io>
2021-08-20 16:24:36 +0000

    (!69) Deploy to GitLab Pages.

--- .github/scripts/github/build.bash
+++ .github/scripts/github/build.bash
@@ -2,9 +2,12 @@
 
 # A script to build the site for GitHub Pages.
 
+# Install `curl`
+apt-get update >/dev/null 2>&1 && apt-get install -y curl >/dev/null 2>&1
+
 # Get Zola
-wget https://github.com/getzola/zola/releases/download/v0.14.0/zola-v0.14.0-x86_64-unknown-linux-gnu.tar.gz >/dev/null 2>&1
-tar -xf zola*.tar.gz && rm zola-*
+curl -L https://github.com/getzola/zola/releases/download/v0.14.0/zola-v0.14.0-x86_64-unknown-linux-gnu.tar.gz > zola.tar.gz 2>/dev/null
+tar -xf zola.tar.gz && rm zola.tar.gz
 
 # Build the site
 if [ ${#} -eq 0 ]; then
@@ -17,8 +20,8 @@ fi
 rm zola
 
 # Get `minify`
-wget https://github.com/tdewolff/minify/releases/download/v2.9.19/minify_linux_amd64.tar.gz >/dev/null 2>&1
-tar -xf minify_* minify && rm minify_*
+curl -L https://github.com/tdewolff/minify/releases/download/v2.9.19/minify_linux_amd64.tar.gz > minify.tar.gz 2>/dev/null
+tar -xf minify.tar.gz minify && rm minify.tar.gz
 
 # Use `minify`
 ./minify -ra -o . public
