7a0480a774fb09e05d0d4aa98cf5ba2a7f9a4d8b
Pavel Sobolev <paveloom@riseup.net>
2021-12-06 13:13:27 +0300

    Update Zola and `minify`.

--- .github/scripts/github/build.bash
+++ .github/scripts/github/build.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# A script to build the site for GitHub Pages.
+# A script to build the site on Ubuntu host
 
 # Install `curl`
 apt-get update >/dev/null 2>&1 && apt-get install -y curl >/dev/null 2>&1
 
 # Get Zola
-curl -L https://github.com/getzola/zola/releases/download/v0.14.1/zola-v0.14.1-x86_64-unknown-linux-gnu.tar.gz > zola.tar.gz 2>/dev/null
+curl -L https://github.com/getzola/zola/releases/download/v0.15.0/zola-v0.15.0-x86_64-unknown-linux-gnu.tar.gz >zola.tar.gz 2>/dev/null
 tar -xf zola.tar.gz && rm zola.tar.gz
 
 # Build the site
@@ -20,7 +20,7 @@ fi
 rm zola
 
 # Get `minify`
-curl -L https://github.com/tdewolff/minify/releases/download/v2.9.19/minify_linux_amd64.tar.gz > minify.tar.gz 2>/dev/null
+curl -L https://github.com/tdewolff/minify/releases/download/v2.9.22/minify_linux_amd64.tar.gz >minify.tar.gz 2>/dev/null
 tar -xf minify.tar.gz minify && rm minify.tar.gz
 
 # Use `minify`
