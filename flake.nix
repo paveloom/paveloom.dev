7a2ef0a4713255c36c06920126cb2b377aa8cff9
Pavel Sobolev <contact@paveloom.dev>
2024-10-12 17:54:14 +0300

    Update the flake dependencies.

--- flake.nix
+++ flake.nix
@@ -8,10 +8,7 @@
     let
       system = "x86_64-linux";
 
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      pkgs = import nixpkgs { inherit system; };
     in
     {
       packages.${system}.site =
