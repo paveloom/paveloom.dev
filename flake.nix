532a6de36841c58360b5fe240a4268a97ef98bbd
Pavel Sobolev <contact@paveloom.dev>
2024-10-12 17:59:10 +0300

    Don't provide a Nix package anymore.

--- flake.nix
+++ flake.nix
@@ -11,19 +11,6 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system}.site =
-        pkgs.runCommandLocal "paveloom.dev"
-          {
-            src = ./src;
-            nativeBuildInputs = with pkgs; [ minify ];
-          }
-          ''
-            cp -r $src out
-            chmod +w -R out
-            ls -l out
-            minify -rav -o . out
-            cp -r out $out
-          '';
       devShells.${system}.default = pkgs.mkShell {
         name = "paveloom.dev-shell";
 
